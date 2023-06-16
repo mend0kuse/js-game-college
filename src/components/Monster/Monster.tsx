0251b6eed80a0866d5ba3d8e33e861074b7dbe26
Васильев Семен Геннадьевич <20054@iat.iat>
2023-06-16 13:29:29 +0800

    Фикс движения монстров в паузе

--- src/components/Monster/Monster.tsx
+++ src/components/Monster/Monster.tsx
@@ -2,7 +2,8 @@ import styles from './Monster.module.scss';
 
 import cn from 'classnames'
 import { IMonster } from '../../types/types';
-import { FC, MutableRefObject, useEffect, useMemo, useRef, useState } from 'react';
+import { FC, MutableRefObject, useContext, useEffect, useMemo, useRef, useState } from 'react';
+import { AppContext } from '../../context/appContext';
 
 interface MonsterProps {
     className: string;
@@ -20,6 +21,8 @@ interface Pos {
 export const Monster: FC<MonsterProps> = (props) => {
     const { className, monster } = props;
 
+    const { pause } = useContext(AppContext)
+
     const timerRef = useRef() as MutableRefObject<ReturnType<typeof setInterval>>;
 
     const [pos, setPos] = useState<Pos>({ left: monster.posX, top: monster.posY })
@@ -28,7 +31,9 @@ export const Monster: FC<MonsterProps> = (props) => {
     const changedValue = useMemo(() => values[Math.floor(Math.random() * 2)], [])
 
     const move = () => {
-        setPos({ ...pos, [changedDirection]: pos[changedDirection] + changedValue })
+        if (!pause) {
+            setPos({ ...pos, [changedDirection]: pos[changedDirection] + changedValue })
+        }
     }
 
     useEffect(() => {
